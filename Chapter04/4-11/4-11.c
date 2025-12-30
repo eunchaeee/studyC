27d9208f335b0a4fde8dae12cbae1120e6fc2abd
eunchae <ddolchae513@gmail.com>
2025-12-30 10:18:42 +0900

    4-11. 콤마 연산자

---
+++ Chapter04/4-11/4-11.c
@@ -0,0 +1,15 @@
+// 4-11. ÄŞ¸¶ ¿¬»êÀÚ
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 10, b = 20;
+	int res;
+
+	res = (++a, ++b);
+
+	printf("a:%d, b:%d\n", a, b);
+	printf("res:%d\n", res);
+
+	return 0;
+}
