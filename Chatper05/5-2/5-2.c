ac59397c49b1e86376b8cc0689570462da2f0184
eunchae <ddolchae513@gmail.com>
2026-01-23 09:21:12 +0900

    5-2. if-else 문의 사용

---
+++ Chatper05/5-2/5-2.c
@@ -0,0 +1,21 @@
+// 5-2. if-else 문의 사용
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 10;
+
+	if (a >= 0)
+	{
+		a = 1;
+	}
+	else
+	{
+		a = -1;
+	}
+
+	printf("a : %d\n", a);
+
+	return 0;
+}
