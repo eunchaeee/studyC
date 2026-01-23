1d4b5f5bc1e8a978570da4fdaa156aad662594d6
eunchae <ddolchae513@gmail.com>
2026-01-23 09:25:23 +0900

    5-3. if ~ else if ~ else 문 사용

---
+++ Chatper05/5-3/5-3.c
@@ -0,0 +1,24 @@
+//5-3. if ~ else if ~ else¹® »ç¿ë
+
+#include <stdio.h>
+
+int main()
+{
+	int a = 0, b = 0;
+
+	if (a > 0)
+	{
+		b = 1;
+	}
+	else if (a == 0)
+	{
+		b = 2;
+	}
+	else
+	{
+		b = 3;
+	}
+
+	printf("b : %d\n", b);
+	return 0;
+}
