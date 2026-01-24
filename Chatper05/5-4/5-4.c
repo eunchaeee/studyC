020c6c4af1ab39d8fd53d50f2d1d69b2175cedc5
eunchae <ddolchae513@gmail.com>
2026-01-24 17:38:25 +0900

    5-4. 중첩된 if문

---
+++ Chatper05/5-4/5-4.c
@@ -0,0 +1,25 @@
+// 5-2. ÁßÃ¸µÈ if ¹®
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 20, b = 10;
+
+	if (a > 10)
+	{
+		if (b >= 0)
+		{
+			b = 1;
+		}
+		else
+		{
+			b = -1;
+		}
+	}
+
+	printf("a : %d, b : %d\n", a, b);
+
+	return 0;
+
+}
