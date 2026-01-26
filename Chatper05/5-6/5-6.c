93ccf5068c4eaf6b2a5f86396f6c22c125839ef9
eunchae <ddolchae513@gmail.com>
2026-01-26 09:42:15 +0900

    5-6. switch-case 문의 사용

---
+++ Chatper05/5-6/5-6.c
@@ -0,0 +1,29 @@
+// 5-6. switch ~ case 문의 사용
+
+#include <stdio.h>
+
+int main(void)
+{
+	int rank = 2, m = 0;
+
+	switch (rank)
+	{
+	case 1:
+		m = 300;
+		break;
+
+	case 2:
+		m = 200;
+		break;
+	case 3:
+		m = 100;
+		break;
+	default:
+		m = 10;
+		break;
+	}
+
+	printf("m = %d\n", m);
+
+	return 0;
+}
